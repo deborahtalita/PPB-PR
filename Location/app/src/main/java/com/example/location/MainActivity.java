2029efb2ac8940be3b9ff41c8f82f5a9d7889ee1
deborahtalita <72002059+deborahtalita@users.noreply.github.com>
2022-05-23 11:35:04 +0700

    remove unused statements

--- Location/app/src/main/java/com/example/location/MainActivity.java
+++ Location/app/src/main/java/com/example/location/MainActivity.java
@@ -30,10 +30,7 @@ public class MainActivity extends AppCompatActivity {
             // You can use the API that requires the permission.
             // performAction(...);
          // } else if (shouldShowRequestPermissionRationale()) {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
+
             // showInContextUI(...);
          } else {
             // Ask for both the ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION permissions.
@@ -42,43 +39,6 @@ public class MainActivity extends AppCompatActivity {
                             Manifest.permission.ACCESS_COARSE_LOCATION },
                     REQUEST_CODE);
         }
-
-//        // Register the permissions callback, which handles the user's response to the
-//        // system permissions dialog. Save the return value, an instance of
-//        // ActivityResultLauncher, as an instance variable.
-//        ActivityResultLauncher<String> requestPermissionLauncher =
-//                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-//                    if (isGranted) {
-//                        // Permission is granted. Continue the action or workflow in your
-//                        // app.
-//                    } else {
-//                        // Explain to the user that the feature is unavailable because the
-//                        // features requires a permission that the user has denied. At the
-//                        // same time, respect the user's decision. Don't link to system
-//                        // settings in an effort to convince the user to change their
-//                        // decision.
-//                    }
-//                });
-//
-//        if (ContextCompat.checkSelfPermission(
-//                this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-//                PackageManager.PERMISSION_GRANTED) {
-//            // You can use the API that requires the permission.
-////            performAction(...);
-////        } else if (shouldShowRequestPermissionRationale(...)) {
-////            // In an educational UI, explain to the user why your app requires this
-////            // permission for a specific feature to behave as expected. In this UI,
-////            // include a "cancel" or "no thanks" button that allows the user to
-////            // continue using your app without granting the permission.
-////            showInContextUI(...);
-////        } else {
-//            // You can directly ask for the permission.
-//            // The registered ActivityResultCallback gets the result of this request.
-//            requestPermissionLauncher.launch(
-//                    new String[] {
-//                            Manifest.permission.ACCESS_FINE_LOCATION,
-//                            Manifest.permission.ACCESS_COARSE_LOCATION});
-//        }
     }
 
     @Override
@@ -106,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_LONG).show();
                 }
                 return;
-            // default: super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
         // Other 'case' lines to check for other
         // permissions this app might request.
